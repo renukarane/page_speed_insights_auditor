bfa2f3389286268e00612ac5c57e97d5241d50a8
boshify <140970186+boshify@users.noreply.github.com>
2023-11-25 18:48:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
-
-import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
-# Function to scrape the sitemap
-def scrape_sitemap(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'xml')
-    urls = [element.text for element in soup.find_all('loc')]
-    return urls
-
-# Function to get PageSpeed Insights
-def get_pagespeed_insights(url, api_key):
-    # Example API request:
-    # https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={url}&key={api_key}
-    response = requests.get(f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed",
-                            params={"url": url, "key": api_key})
-    return response.json()
-
 # Streamlit app
 def main():
     st.title("Sitemap Scraper and PageSpeed Insights")
     input_url = st.text_input("Enter the URL of the sitemap (e.g., https://example.com/sitemap.xml):")
-    api_key = st.text_input("Enter your Google PageSpeed Insights API Key:")
+
+    # Retrieve the API key from Streamlit secrets
+    api_key = st.secrets["pagespeed"]["api_key"]
     
     if st.button("Scrape and Analyze"):
-        if input_url and api_key:
+        if input_url:
             scraped_urls = scrape_sitemap(input_url)
             results = []
             for url in scraped_urls:
